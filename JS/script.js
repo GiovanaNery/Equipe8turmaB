116b045bc85e555a18348c9b951f09d6250cbe67
taviman <otavioblancopa@gmail.com>
2023-12-05 15:35:15 -0300

    add info do roberto campos

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+// script.js
+function toggleNightMode() {
+    const body = document.body;
+
+    // Alternar entre as classes 'day-mode' e 'night-mode'
+    body.classList.toggle('day-mode');
+    body.classList.toggle('night-mode');
+}
