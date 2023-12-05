cfb31cf2902b28630b67cb6195ca271c95ce0c70
GiovanaNery <130416807+GiovanaNery@users.noreply.github.com>
2023-12-05 16:09:36 -0300

    codigo finalizado

--- JS/script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-function toggleNightMode() {
-    const body = document.body;
-
-    // Alternar entre as classes 'day-mode' e 'night-mode'
-    body.classList.toggle('day-mode');
-    body.classList.toggle('night-mode');
-}
